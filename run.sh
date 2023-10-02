6504347227e8dfcd61efff992257e942a7253129
Abdelilah Essiari <aessiari@lbl.gov>
2023-10-02 12:36:52 -0700

    testing with ansible

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-ls -l /tmp
+ansible --version
+terraform --version
+
 echo "GEN_SSH_KEYS=$GEN_SSH_KEYS"
+ls -l /tmp
 
 go-deploy -init --working-directory aws
 go-deploy --working-directory aws -w test-go-deploy -c config.yml.sample
 public_ip=`terraform -chdir=aws output -raw public_ip`
-which ansible
-ansible --version
 echo $public_ip
-#ssh -i /tmp/go-ssh ubuntu@$public_ip ls -l stage_dir/test_file
+ssh -i /tmp/go-ssh ubuntu@$public_ip ls -l stage_dir/test_file
 ret=$?
 go-deploy --working-directory aws -w test-go-deploy -destroy
 exit $ret 
