8ea465a89822c42b9c094477850bd4852b4e3aec
Abdelilah Essiari <aessiari@lbl.gov>
2023-10-02 03:49:51 -0700

    testing actions

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ls -l /tmp
+echo "GEN_SSH_KEYS=$GEN_SSH_KEYS"
 
 go-deploy -init --working-directory aws
 go-deploy --working-directory aws -w production-2023-10-01 -c config.yml.sample
