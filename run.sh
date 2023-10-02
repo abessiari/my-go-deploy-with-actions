026d9c7cc004a82a37d9bd829603addbcc66ade9
Abdelilah Essiari <aessiari@lbl.gov>
2023-10-02 12:16:12 -0700

    testing build

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ls -l /tmp
+echo "GEN_SSH_KEYS=$GEN_SSH_KEYS"
+
+go-deploy -init --working-directory aws
+go-deploy --working-directory aws -w test-go-deploy -c config.yml.sample
+public_ip=`terraform -chdir=aws output -raw public_ip`
+echo $public_ip
+ssh -i /tmp/go-ssh ubuntu@$public_ip ls -l stage_dir/test_file
+ret=$?
+go-deploy --working-directory aws -w test-go-deploy -destroy
+exit $ret 
